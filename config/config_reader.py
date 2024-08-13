40010fd42151888fc1462f837a25ac64bc8d3c3e
arzw11 <166683065+arzw11@users.noreply.github.com>
2024-08-13 11:40:28 +0700

    bug fixes

--- config/config_reader.py
+++ config/config_reader.py
@@ -2,10 +2,10 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import SecretStr
 
 class Settings(BaseSettings):
-    bot_token: SecretStr
-    owner: SecretStr
-    cwc: SecretStr
-    db_lite: str
     model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
+    bot_token: SecretStr
+    owner: int
+    cwc: int
+    db_lite: str
 
 config = Settings()
